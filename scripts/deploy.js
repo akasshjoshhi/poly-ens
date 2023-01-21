5f7dba2e1cebcbfa50e8ea6ba662456bed453cf0
ChidAkash <akash.joshi@iamhere.mobi>
2023-01-21 14:49:53 +0530

    supreme domain in deploy file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,16 +5,16 @@ const main = async () => {
   
     console.log("Contract deployed to:", domainContract.address);
   
-    let txn = await domainContract.register("kaizoku",  {value: hre.ethers.utils.parseEther('0.1')});
+    let txn = await domainContract.register("supreme",  {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
-    console.log("Minted domain kaizoku.king");
+    console.log("Minted domain supreme.king");
   
-    txn = await domainContract.setRecord("kaizoku", "Am I a kaizoku or a king?");
+    txn = await domainContract.setRecord("supreme", "king or not live life supreme");
     await txn.wait();
-    console.log("Set record for kaizoku.king");
+    console.log("Set record for supreme.king");
   
-    const address = await domainContract.getAddress("kaizoku");
-    console.log("Owner of domain kaizoku:", address);
+    const address = await domainContract.getAddress("supreme");
+    console.log("Owner of domain supreme:", address);
   
     const balance = await hre.ethers.provider.getBalance(domainContract.address);
     console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
